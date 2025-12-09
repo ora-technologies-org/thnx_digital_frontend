deb75b32ec72f851d083015b3553fca445fb1977
dbipin <bipin.dhimal1@gmail.com>
2025-12-09 17:49:43 +0545

    fix - proxy for local testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,5 +12,11 @@ export default defineConfig({
   server: {
     port: 8080,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://thnxdigital.com',
+        changeOrigin: true,
+      }
+    }
   },
 });
