95d69a0e9b9763538bc248e952b6c7dbcf94eeaa
Sital Dulal <sheetaldulal@oratechnologies.io>
2025-12-16 16:38:16 +0545

    feat: add merchant profile status and enable feature as well

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,26 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../features/auth/slices/authSlice';
-import giftCardReducer from '../features/giftCards/slices/giftCardSlice';
-
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/slices/authSlice";
+import giftCardReducer from "../features/giftCards/slices/giftCardSlice";
+import profileReducer from "../features/auth/slices/profileSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     giftCards: giftCardReducer,
+    profile: profileReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
-      devTools: process.env.NODE_ENV !== 'production',
-
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   store.subscribe(() => {
-    console.log('🏪 Redux State Updated:', store.getState().auth);
+    console.log("🏪 Redux State Updated:", store.getState().auth);
   });
 }
